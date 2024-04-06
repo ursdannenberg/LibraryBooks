042c8e5e12fc0573b616facce5ffb03984b1f268
ursdannenberg <ur.schaefer@live.de>
2024-04-06 10:23:32 +0200

    Remove loan_date form book model and fix an issue with it.

--- librarysite/library/models.py
+++ librarysite/library/models.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    loan_date = models.DateTimeField("Loan date")
     return_date = models.DateTimeField("Return date")
     library = models.CharField(max_length=200)
     
@@ -14,4 +13,4 @@ class Book(models.Model):
         return self.title
     
     def return_next_week(self):
-        return self.return_date <= timezone.now() + datetime.timedelta(week=1)
+        return self.return_date <= timezone.now() + datetime.timedelta(days=7)

e55454a9d5f87b1ca764cad76c5938d07e9c5a24
ursdannenberg <ur.schaefer@live.de>
2024-04-06 12:19:18 +0200

    Add list of books as first view.

--- librarysite/library/views.py
+++ librarysite/library/views.py
@@ -1,3 +1,14 @@
 #from django.shortcuts import render
+from django.views import generic
+from .models import Book
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = "library/index.html"
+    context_object_name = "book_list"
+
+    def get_queryset(self):
+        """
+        Return all list of all books.
+        """
+        return Book.objects.all()

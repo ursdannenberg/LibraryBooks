dfd80b5dfe3a8cde24d8192b6931673ada6eab6b
ursdannenberg <ur.schaefer@live.de>
2024-04-06 10:24:09 +0200

    Add tests for return_next_week method of book model.

--- librarysite/library/tests.py
+++ librarysite/library/tests.py
@@ -1,3 +1,32 @@
-#from django.test import TestCase
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Book
 
 # Create your tests here.
+def create_book(return_date):
+    """
+    Create a book with the given `return_date`.
+    """
+    return Book.objects.create(title='Title', return_date=return_date, library='Library')
+
+class BookModelTests(TestCase):
+    def test_return_next_week_with_old_book(self):
+        """
+        return_next_week() returns True for books whose return_date
+        is within one week.
+        """
+        date = timezone.now() + datetime.timedelta(days=6, hours=23)
+        old_book = create_book(return_date=date)
+        self.assertIs(old_book.return_next_week(), True)
+        
+    def test_return_next_week_with_new_book(self):
+        """
+        return_next_week() returns False for books whose return_date
+        is not within one week.
+        """
+        date = timezone.now() + datetime.timedelta(days=7, hours=1)
+        new_book = create_book(return_date=date)
+        self.assertIs(new_book.return_next_week(), False)
